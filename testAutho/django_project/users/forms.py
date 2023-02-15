d9061f435ae57350581e53f1410e7887808e7797
eldervald <nikolay.ignatenko@outlook.com>
2023-02-15 18:19:50 +0300

    Updated login form

--- testAutho/django_project/users/forms.py
+++ testAutho/django_project/users/forms.py
@@ -21,8 +21,8 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Login'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
     video = None
 
 
